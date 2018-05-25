cb9a582cbb88ec1f1579f1b44a30e2fb4ff500f3
Sheena B <sheena.myb@gmail.com>
2018-05-25 21:18:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value;
   return obj;
 }
+
+function deleteFromObjectByKey(obj, key, value) {
+  return Object.assign({}, obj, key:[value]);
+}
